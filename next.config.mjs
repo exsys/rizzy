9901f4c2d86cdf0bf3c47eb16ec432dd2e4b9805
exsyss <exsysb@gmail.com>
2024-05-11 21:59:10 +0200

    removed next config settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    distDir: 'dist',
-    trailingSlash: true,
-    assetPrefix: '/',
-    images: { unoptimized: true },
     sassOptions: {
         includePaths: ["styles"],
     },
